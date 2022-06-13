97a52a46de38a99a0a9383ca8ef2f9c5e54f0f91
방우현 <docter4@naver.com>
2022-06-13 13:29:06 +0900

    06/13 record

---
+++ level1JAVA/src/DividedZero.java
@@ -0,0 +1,13 @@
+class DividedZero {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DividedZero dividedZero = new DividedZero();
+        int[] arr = {5, 9, 7, 10};
+        int divisor = 5;
+        int[] result = dividedZero.solution(arr, divisor);
+    }
+}
