08168d994590081635cffa10549a48293a4c8ed0
rptbley <docter4@naver.com>
2022-08-09 23:46:42 +0900

    08-09

--- level2JAVA/src/CompressionString.java
+++ level2JAVA/src/CompressionString.java
@@ -5,6 +5,8 @@ class CompressionString {
     public int solution(String s) {
         int answer = 0;
 
+        if(s.length() == 1) return 1;
+
         boolean isEven = s.length() % 2 == 0;
         int divideNum = isEven ? s.length() / 2 : s.length() / 3;
 
@@ -29,7 +31,9 @@ class CompressionString {
         for(int i = 0; i < s.length(); i += divideNum) {
             int endIndex = divideNum + i;
 
-            if(endIndex > s.length()) break;
+            if(endIndex > s.length()) {
+                endIndex = s.length();
+            }
 
             String parsedStr = s.substring(i, endIndex);
             parsedStrList.add(parsedStr);
@@ -42,13 +46,13 @@ class CompressionString {
         int dupleNum = 1;
         StringBuilder result = new StringBuilder();
         String currentStr = "";
-
+        System.out.println("parsedStrList = " + parsedStrList);
         for(int i = 0; i < parsedStrList.size(); i++) {
             if(i == 0) {
                 currentStr = parsedStrList.get(i);
             }
-            
-            if(i + 1 < parsedStrList.size() - 1) {
+
+            try {
                 String nextStr = parsedStrList.get(i + 1);
 
                 if(currentStr.equals(nextStr)) {
@@ -58,10 +62,8 @@ class CompressionString {
                     dupleNum = 1;
                     currentStr = nextStr;
                 }
-            } else {
-                if(!parsedStrList.get(i).equals(currentStr)) {
-                    result.append(parsedStrList.get(i));
-                }
+            } catch (Exception e) {
+                result.append(dupleNum > 1 ? dupleNum + currentStr : currentStr);
             }
         }
         System.out.println("result = " + result);
@@ -70,7 +72,7 @@ class CompressionString {
 
     public static void main(String[] args) {
         CompressionString compressionString = new CompressionString();
-        int result = compressionString.solution("ababcdcdababcdcd");
+        int result = compressionString.solution("xxxxxxxxxxyyy");
         System.out.println("result = " + result);
     }
 }
