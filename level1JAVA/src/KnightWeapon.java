f35d3d9d320c26d37d2e137984f4c1fdcf17d7ca
방우현 <docter4@naver.com>
2023-01-06 16:11:07 +0900

    기사단원의 무기(푸는 중)

---
+++ level1JAVA/src/KnightWeapon.java
@@ -0,0 +1,12 @@
+class KnightWeapon {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        KnightWeapon knightWeapon = new KnightWeapon();
+        int result = knightWeapon.solution(5, 3, 2);
+        System.out.println("result = " + result);
+    }
+}
