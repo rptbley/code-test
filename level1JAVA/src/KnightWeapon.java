0352ac7db0d5d901434e3bbe026f7fd9b63a4d1e
방우현 <docter4@naver.com>
2023-01-09 18:01:16 +0900

    기사단원읜 무기(푸는 중)

--- level1JAVA/src/KnightWeapon.java
+++ level1JAVA/src/KnightWeapon.java
@@ -1,12 +1,24 @@
 class KnightWeapon {
     public int solution(int number, int limit, int power) {
         int answer = 0;
+
+        for(int i = 1; i <= number; i++) {
+            int sumOfGCD = 0;
+
+            for(int k = 1; k <= i; k++) {
+                if(i % k == 0) {
+                    sumOfGCD++;
+                }
+            }
+
+            answer += sumOfGCD > limit ? power : sumOfGCD;
+        }
         return answer;
     }
 
     public static void main(String[] args) {
         KnightWeapon knightWeapon = new KnightWeapon();
-        int result = knightWeapon.solution(5, 3, 2);
+        int result = knightWeapon.solution(10, 3, 2);
         System.out.println("result = " + result);
     }
 }
