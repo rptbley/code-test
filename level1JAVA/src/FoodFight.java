455e376e5defad5d2ed2bf36cd443c0a8662ce97
방우현 <docter4@naver.com>
2023-01-10 10:46:47 +0900

    푸드 파이트 대회

---
+++ level1JAVA/src/FoodFight.java
@@ -0,0 +1,12 @@
+class FoodFight {
+    public String solution(int[] food) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FoodFight foodFight = new FoodFight();
+        String result = foodFight.solution(new int[]{1,3,4,6});
+        System.out.println("result = " + result);
+    }
+}
