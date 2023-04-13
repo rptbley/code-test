bf98f3a59142e65dff2d4f7b1ca6e206a1eb7c32
방우현 <docter4@naver.com>
2023-04-14 01:54:38 +0900

    test

---
+++ level1JAVA/src/MakingHamburger.java
@@ -0,0 +1,19 @@
+class MakingHamburger {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        MakingHamburger makingHamburger = new MakingHamburger();
+        int result = makingHamburger.solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1});
+        System.out.println("result = " + result);
+    }
+}
+
+// 1 = 빵
+// 2 = 야채
+// 3 = 고기
