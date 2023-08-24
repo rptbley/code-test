f17e90fbe7b78bb3399e42b7164a73630a9d88df
방우현 <docter4@naver.com>
2023-08-24 17:49:34 +0900

    2023/08/24  Problem: Partner  - Status: solving

---
+++ level1JAVA/src/Kbti.java
@@ -0,0 +1,12 @@
+public class Kbti {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
+        int[] choices = {5, 3, 2, 7, 5};
+        System.out.println("solution = " + new Kbti().solution(survey, choices));
+    }
+}
