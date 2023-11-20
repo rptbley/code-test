039b2bdb103878dda6b27fb4157bad4293b4fbf3
방우현 <docter4@naver.com>
2023-11-20 21:54:27 +0900

    solving bakjoon

---
+++ level2JAVA/src/SumOfSequence.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SumOfSequence {
+    public static int[] Solution(int[] sequence, int k) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] sequence = {1,2,3,4,5};
+        int k = 7;
+        System.out.println("solution = " + Arrays.toString(SumOfSequence.Solution(sequence, k)));
+    }
+}
